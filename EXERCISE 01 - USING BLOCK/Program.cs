6b01f51a1b507b6710580fb4670dbbb4a520e510
Marcus Bianchi <82729031+CmdBianchi@users.noreply.github.com>
2021-05-21 16:22:12 -0300

    V.2

--- EXERCISE 01 - USING BLOCK/Program.cs
+++ EXERCISE 01 - USING BLOCK/Program.cs
@@ -8,13 +8,11 @@ namespace EXERCISE_01___USING_BLOCK {
             string path = @"c:\temp\file1.txt";
 
             try { 
-                using (FileStream fs = new FileStream(path, FileMode.Open)) {
-                    using (StreamReader sr = new StreamReader(fs)) {
+                using (StreamReader sr = File.OpenText(path)) {
                         while (!sr.EndOfStream) {
                             string line = sr.ReadLine();
                             Console.WriteLine(line);
-                        }
-                    }
+                        }                    
                 }
             }
             catch(IOException e) {
